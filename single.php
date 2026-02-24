78fdd636b40fe9556b32b36940f83d8f44cd4ccd
kartikcool15 <kartikcool15@users.noreply.github.com>
2026-02-24 14:40:16 +0530

    Add Rank Math breadcrumbs support to post hero section

--- single.php
+++ single.php
@@ -15,6 +15,7 @@
         <div class="post-hero">
             <div class="post-hero-content">
                 <nav class="breadcrumb">
+                    <?php if (function_exists("rank_math_the_breadcrumbs")) rank_math_the_breadcrumbs(); ?>
                     <a href="<?php echo esc_url(home_url('/')); ?>">
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" style="vertical-align: middle; margin-right: 4px;">
                             <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
