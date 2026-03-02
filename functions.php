cf672f34d67bceccea31b48322bfa01071dae69b
kartikcool15 <kartikcool15@users.noreply.github.com>
2026-03-02 12:26:13 +0530

    fix: disable block library CSS and emoji scripts to improve performance

--- functions.php
+++ functions.php
@@ -92,6 +92,29 @@ function kiosk_add_resource_hints($urls, $relation_type)
 }
 add_filter('wp_resource_hints', 'kiosk_add_resource_hints', 10, 2);
 
+// Disable block library CSS
+function kiosk_remove_block_library_css()
+{
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-block-style'); // WooCommerce blocks if present
+    wp_dequeue_style('global-styles'); // Global styles
+}
+add_action('wp_enqueue_scripts', 'kiosk_remove_block_library_css', 100);
+
+// Disable emoji scripts and styles
+function kiosk_disable_emojis()
+{
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+}
+add_action('init', 'kiosk_disable_emojis');
+
 /**
  * Bulk process existing posts to assign education taxonomy
  * Run this once via URL: yoursite.com/?process_education_taxonomy=1
