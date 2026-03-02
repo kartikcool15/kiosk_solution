94a4ae12aa64ba001ae59f13ac8881a7177ece4e
kartikcool15 <kartikcool15@users.noreply.github.com>
2026-03-02 12:10:33 +0530

    fix: remove unused title tag and ads script from header

--- header.php
+++ header.php
@@ -4,12 +4,9 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9103495719984418"
-        crossorigin="anonymous"></script>
     <?php wp_head(); ?>
 
     <!-- Google tag (gtag.js) -->
