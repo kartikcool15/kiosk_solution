84404c21ecd3464058710c3599fe8a9a933cda5c
kartikcool15 <kartikcool15@users.noreply.github.com>
2026-03-02 12:38:05 +0530

    fix: move Google Fonts stylesheet link above wp_head for improved loading

--- header.php
+++ header.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-1RSRP789WR"></script>
     <script>
